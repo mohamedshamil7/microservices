339af9105d33cce390e01fc9409f182f2991e215
mohamedshamil7 <mohamedshamil0507@gmail.com>
2023-07-04 12:39:26 +0530

    Cluster Created Cluster Ip created Node Port Created Getting api calls

--- posts/index.js
+++ posts/index.js
@@ -20,7 +20,7 @@ app.post('/posts',async(req,res)=>{
     posts[id]= {
         id,title
     };
-   await axios.post("http://localhost:4005/events",{
+   await axios.post("http://event-bus-srv:4005/events",{
         type:"postCreated",
         data:{
             id,title
@@ -38,5 +38,6 @@ app.post("/events",(req,res)=>{
 
 
 app.listen(4000,()=>{
+    console.log("version v 40")
     console.log("listening on port 4000")
 })
